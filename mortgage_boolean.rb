36b36345923dd2c3f5cf8b2038f12225d0844e59
Manjit Singh <manjitsingh23@gmail.com>
2014-02-24 09:15:54 +0000

    Initial Mortgage Code

---
+++ mortgage_boolean.rb
@@ -0,0 +1,12 @@
+def provide_mortgage?(salary, deposit, property_value)
+  loan_amount = property_value - deposit
+  min_deposit = 0.2 # 20%
+  max_multiplier = 5 # how many annual incomes can be borrowed
+  deposit >= property_value * min_deposit && 
+      salary * max_multiplier >= loan_amount
+end
+
+puts provide_mortgage?(25000, 30000, 150000)  
+
+
+
